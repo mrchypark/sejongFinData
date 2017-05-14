4321bf399b189f7ade5b85c4ade50b4713463951
Chan-Yub Park <mrchypark@gmail.com>
2017-05-14 15:14:08 +0900

    Update getdata.R

--- getdata.R
+++ getdata.R
@@ -3,7 +3,7 @@ if (!require("stringi")) install.packages("stringi")
 library(rvest)
 library(stringi)
 
-print("start get code. mrchypark")
+print("start get code. mrchypark  go")
 
 url <- "http://www.sejongdata.com/query/value.html"
 
